fb5e9ad608c146e6ccd49a8925e06344958cf33a
ToshaJE <toshaester25@gmail.com>
2025-05-06 00:30:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.use(express.static('public'));
 
 const port = process.env.PORT ||8000
 app.listen(port,()=>{
-    console.log('listening on port ${port}');
+    console.log('listening on port '+"http://localhost:"+port);
 })
 
